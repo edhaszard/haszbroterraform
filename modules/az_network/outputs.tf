4a5822554f00be9d099a80c6683f439d9599f6e9
edhaszard <ed.haszard@ihc.org.nz>
2021-10-08 12:29:27 +1300

    deleting and starting from scratch

--- modules/az_network/outputs.tf
+++
@@ -1,9 +0,0 @@
-
-output "subnet1_id" {
-  
-  value = data.azurerm_subnet.subnet1.id
-}
-
-output "subnet1_name" {
-  value = var.subnet1_name
-}
--- modules/az_windows_vm/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "vm_ids" {
-  value = azurerm_windows_virtual_machine.vm[0].id
-}
-
-output "network_interface_id" {
-  value = azurerm_network_interface.net_if[0].id
-}
-
