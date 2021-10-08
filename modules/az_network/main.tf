451507d3d762c04e104f3fe0d2ca6ab976adfa5c
edhaszard <ed.haszard@ihc.org.nz>
2021-10-08 18:39:34 +1300

    added network_resource_group_name

--- modules/az_network/main.tf
+++ modules/az_network/main.tf
@@ -4,6 +4,6 @@
 resource "azurerm_virtual_network" "vnet1" {
     name = var.vnet1_name
     location = var.location
-    resource_group_name = var.location
+    network_resource_group_name = var.location
     address_space = var.vnet1_addr_space
 }
