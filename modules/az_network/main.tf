4494498e423738c1c2cca60d5401d03459208637
edhaszard <ed.haszard@ihc.org.nz>
2021-10-08 18:37:07 +1300

    removed extraneous '#'

--- modules/az_network/main.tf
+++ modules/az_network/main.tf
@@ -1,7 +1,7 @@
 ##########################################################
 ## Vnet
 ##########################################################
-#resource "azurerm_virtual_network" "vnet1" {
+resource "azurerm_virtual_network" "vnet1" {
     name = var.vnet1_name
     location = var.location
     resource_group_name = var.location
