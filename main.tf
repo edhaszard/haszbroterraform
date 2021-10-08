86a4aa792897575c979c3195ad56fd07ddd4458c
edhaszard <ed.haszard@ihc.org.nz>
2021-10-08 16:11:26 +1300

    properly referenced the cloud variable in main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "azurerm_resource_group" "rg1" {
 }
 
 resource "azurerm_virtual_network" "vnet1" {
-  name                = vnet1_name
+  name                = var.vnet1_name
   address_space       = ["10.0.0.0/16"]
   location            = var.default_location
   resource_group_name = azurerm_resource_group.rg1.name
