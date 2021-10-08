94ebd571447fba4346f5a9b050a272695a91b71d
edhaszard <ed.haszard@ihc.org.nz>
2021-10-08 17:59:24 +1300

    changed resource group name source

--- main.tf
+++ main.tf
@@ -56,6 +56,6 @@ module "resourcegr2" {
 resource "azurerm_virtual_network" "vnet1" {
   name = var.vnet1_name
   location = var.default_location
-  resource_group_name = module.resourcegr2.name
+  resource_group_name = var.resource_group2_name
   address_space = var.vnet1_addr_space
 }
