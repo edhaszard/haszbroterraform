3f0bae66297cdd859c0c406ade334541117e8125
edhaszard <ed.haszard@ihc.org.nz>
2021-10-08 19:57:35 +1300

    is this it?

--- modules/az_network/main.tf
+++ modules/az_network/main.tf
@@ -4,6 +4,6 @@
 resource "azurerm_virtual_network" "vnet1" {
     name = var.vnet_name
     location = var.location
-    network_resource_group_name = var.network_resource_group_name
+    resource_group_name = var.network_resource_group_name
     address_space = var.vnet_addr_space
 }
