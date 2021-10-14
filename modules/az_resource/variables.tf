e9c0109297c1197ad3eb350b4f541dc2f2b8d914
edhaszard <ed.haszard@ihc.org.nz>
2021-10-14 16:51:49 +1300

    remmed out location variable az_resource module

--- modules/az_resource/variables.tf
+++ modules/az_resource/variables.tf
@@ -2,12 +2,12 @@ variable "resource_group_name" {
   description = "Name of the resource group."
 }
 
-variable "location" {
+#variable "location" {
   description = "Location of the cluster."
   default     = "westeurope"
-}
-variable "tag_project" {
-}
+#}
+#variable "tag_project" {
+#}
 variable "tag_environment" {
 }
 variable "tag_comments" {
