77243082452a67b001fcc8484bb9523a54b685ff
edhaszard <ed.haszard@ihc.org.nz>
2021-10-15 14:05:52 +1300

    added subnet2 to network main.tf

--- modules/az_network/main.tf
+++ modules/az_network/main.tf
@@ -1,5 +1,5 @@
 ##########################################################
-## Vnet
+## Vnet 1
 ##########################################################
 resource "azurerm_virtual_network" "vnet1" {
     name = var.vnet_name
@@ -9,11 +9,21 @@ resource "azurerm_virtual_network" "vnet1" {
 }
 
 ##########################################################
-## Subnet
+## Subnet 1
 ##########################################################
 resource "azurerm_subnet" "subnet1" {
     name = var.subnet1_name
     virtual_network_name = azurerm_virtual_network.vnet1.name
     resource_group_name = var.network_resource_group_name
     address_prefixes = var.subnet1_prefix
+}
+
+##########################################################
+## Subnet 2
+##########################################################
+resource "azurerm_subnet" "subnet2" {
+    name = var.subnet2_name
+    virtual_network_name = azurerm_virtual_network.vnet1.name
+    resource_group_name = var.network_resource_group_name
+    address_prefixes = var.subnet2_prefix
 }
